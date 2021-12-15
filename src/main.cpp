b90145ee048f36408171abb7b1480b258234266f
okritvik <okritvik@gmail.com>
2021-12-14 21:41:21 -0500

    little modifications

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv){
     sar.get_targets(); //calls the get_targets function of sar object to store the target locations from rosparam server.
     sar.search(); //initiates the search operation
     sar.rescue(); //initiates the rescue operation
-    time_t end_time = time(NULL); //time object to know the end of the simuation
-    std::cout << "Total Simulation Time: " << (end_time - start_time) / 60 <<"\n"; //calculates the total simulation time in minutes and displays that it console
+    time_t end_time = time(NULL); //time object to know the end time of the simuation
+    std::cout << "Total Simulation Time {minutes}: " << (end_time - start_time) / 60 <<"\n"; //calculates the total simulation time in minutes and displays that in console
     return 0;
 }
